de90e5e19da77365af18cdf8ff9deac08e7a1143
Keunhong Park <kpar@cs.washington.edu>
2017-06-14 16:33:02 -0700

    Update sift flow.

--- siftflow.m
+++ siftflow.m
@@ -1,11 +1,11 @@
 function [vx, vy] = siftflow(im1_path, im2_path)
 padsize = 100;
-scale = 0.15;
-im1 = padarray(imread(im1_path), padsize);
-im2 = padarray(imread(im2_path), padsize);
+scale = 0.25;
+im1 = padarray(rgb2gray(imread(im1_path)), padsize);
+im2 = padarray(rgb2gray(imread(im2_path)), padsize);
 
-im1 = imresize(imfilter(im1, fspecial('gaussian',7,1.),'same','replicate'), scale, 'bicubic');
-im2 = imresize(imfilter(im2, fspecial('gaussian',7,1.),'same','replicate'), scale, 'bicubic');
+im1 = imresize(imfilter(im1, fspecial('gaussian',10, 1.),'same','replicate'), scale, 'bicubic');
+im2 = imresize(imfilter(im2, fspecial('gaussian',10, 1.),'same','replicate'), scale, 'bicubic');
 
 im1 = im2double(im1);
 im2 = im2double(im2);
@@ -21,7 +21,7 @@ SIFTflowpara.d = 40*255;
 SIFTflowpara.gamma = 0.005*255;
 SIFTflowpara.nlevels = 4;
 SIFTflowpara.wsize = 2;
-SIFTflowpara.topwsize = 10;
+SIFTflowpara.topwsize = 8;
 SIFTflowpara.nTopIterations = 60;
 SIFTflowpara.nIterations = 30;
 
